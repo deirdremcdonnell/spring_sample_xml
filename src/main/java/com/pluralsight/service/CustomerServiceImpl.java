203b0985bfcd74893e2ac3d88f241791bd2ea501
McDond5 <deirdre.mcdonnell@emc.com>
2017-07-06 11:19:46 +0100

    autowire constructor

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -12,9 +12,9 @@ public class CustomerServiceImpl implements CustomerService {
     //private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl(); //hardcoded ref
     private CustomerRepository customerRepository;
 
-//    public CustomerServiceImpl(CustomerRepository customerRepository) {
-//        this.customerRepository = customerRepository;
-//    }
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public void setCustomerRepository(CustomerRepository customerRepositoryd) {
         this.customerRepository = customerRepositoryd;
